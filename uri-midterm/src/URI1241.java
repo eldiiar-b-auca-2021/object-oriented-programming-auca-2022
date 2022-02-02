48332bab9c46e38dc77b7430cae1e46f96a890f1
eldiiar_b <eldiiar_b@auca.kg>
2022-02-02 20:05:05 +0600

    URI1241 Run time error

---
+++ uri-midterm/src/URI1241.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class URI1241 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        int num = in.nextInt();
+        while(i < num) {
+            String a = in.next();
+            String b = in.next();
+            if (a.endsWith(b)) {
+                System.out.println("encaixa");
+            } else {
+                System.out.println("nao encaixa");
+            }
+            i++;
+        }
+
+    }
+}
