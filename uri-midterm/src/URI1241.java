86b1724581e619923a8dcfc6e491ac0154a81853
eldiiar_b <eldiiar_b@auca.kg>
2022-02-03 12:24:52 +0600

    URI1241 wrong answer

--- uri-midterm/src/URI1241.java
+++ uri-midterm/src/URI1241.java
@@ -5,16 +5,27 @@ public class URI1241 {
         Scanner in = new Scanner(System.in);
         int i = 0;
         int num = in.nextInt();
-        while(i < num) {
-            String a = in.next();
-            String b = in.next();
-            if (a.endsWith(b)) {
+        while (i < num) {
+            String a = " ";
+            String b = " ";
+            char[] firstChar = in.next().toCharArray();
+            char[] secondChar = in.next().toCharArray();
+            for (int j = 0; j < firstChar.length; j++) {
+                if (j >= firstChar.length-2) {
+                  a+=firstChar[j];
+                }
+            }
+            for (int k = 0; k < secondChar.length; k++) {
+                if (k >= secondChar.length-2) {
+                  b+=secondChar[k];
+                }
+            }
+            if (a.equals(b)) {
                 System.out.println("encaixa");
             } else {
                 System.out.println("nao encaixa");
             }
             i++;
         }
-
     }
 }
