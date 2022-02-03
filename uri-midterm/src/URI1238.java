c3cfea84381a4e9a9eeee320a2ffdcb3702c9aae
eldiiar_b <eldiiar_b@auca.kg>
2022-02-03 13:31:30 +0600

    URI1238 run time error

---
+++ uri-midterm/src/URI1238.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class URI1238 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n;
+        int i = 0;
+        while (i < (in.nextInt())) {
+            char[] firstLetters = in.next().toCharArray();
+            char[] secondLetters = in.next().toCharArray();
+            String result = " ";
+            for (int j = 0; j < (firstLetters.length+secondLetters.length); j++) {
+                if (j < firstLetters.length) {
+                    result += firstLetters[j];
+                }
+                if (j < secondLetters.length) {
+                    result += secondLetters[j];
+                }
+            }
+            System.out.println(result);
+            i++;
+        }
+    }
+}
