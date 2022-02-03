8d4886b4dd860c5ade3d267ead542697747f5505
eldiiar_b <eldiiar_b@auca.kg>
2022-02-03 14:41:52 +0600

    URI1241 accepted

--- uri-midterm/src/URI1241.java
+++ uri-midterm/src/URI1241.java
@@ -6,25 +6,14 @@ public class URI1241 {
         int i = 0;
         int num = in.nextInt();
         while (i < num) {
-            String a = " ";
-            String b = " ";
-            char[] firstChar = in.next().toCharArray();
-            char[] secondChar = in.next().toCharArray();
-            for (int j = 0; j < firstChar.length; j++) {
-                if (j >= firstChar.length-2) {
-                  a+=firstChar[j];
-                }
-            }
-            for (int k = 0; k < secondChar.length; k++) {
-                if (k >= secondChar.length-2) {
-                  b+=secondChar[k];
-                }
-            }
-            if (a.equals(b)) {
+            String a = in.next();
+            String b = in.next();
+            if (a.endsWith(b)) {
                 System.out.println("encaixa");
             } else {
                 System.out.println("nao encaixa");
             }
+
             i++;
         }
     }
