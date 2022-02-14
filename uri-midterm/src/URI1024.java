6fb08d483f39ec4fea6bc89b6656a1925f9c1b98
eldiiar_b <eldiiar_b@auca.kg>
2022-02-14 21:08:33 +0600

    URI10124 time limit exceeded

---
+++ uri-midterm/src/URI1024.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class URI1024 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+           char[] a = in.nextLine().toCharArray();
+           for (int j = 0; j < a.length; j++) {
+               int decimal = (a[j]);
+               if (decimal >= 65 && decimal <= 90 || decimal >= 97 && decimal <= 122){
+                   a[j] = (char) (decimal+3);
+               }
+           }
+           for (int k = 0;  k < a.length/2; k++) {
+               char temp = a[k];
+               a[k] = a[a.length-1-k];
+               a[a.length-1-k] = temp;
+           }
+           for (int k = a.length/2; k < a.length; k++) {
+              a[k] = (char) (a[k]-1);
+
+           }
+           for (int k = 0; k < a.length; k++) {
+               System.out.print(a[k]);
+           }
+        }
+    }
+}
