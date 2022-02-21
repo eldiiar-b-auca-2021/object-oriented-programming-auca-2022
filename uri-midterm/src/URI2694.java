95f032d13e9c0445616f2473afb208c1c1181f45
eldiiar_b <eldiiar_b@auca.kg>
2022-02-21 22:41:59 +0600

    URI1294 accepted

---
+++ uri-midterm/src/URI2694.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class URI2694 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        int n = in.nextInt();
+        while (i < n) {
+            String letter  = in.next();
+            String temp = "0";
+            int sum = 0;
+            for (int j = 0; j < letter.length(); j++) {
+                char ch = letter.charAt(j);
+                if (Character.isDigit(letter.charAt(j))) {
+                    temp += ch;
+                } else {
+                    sum += Integer.parseInt(temp);
+                }
+                temp = "0";
+            }
+            System.out.println(sum += Integer.parseInt(temp));
+            i++;
+        }
+    }
+}
