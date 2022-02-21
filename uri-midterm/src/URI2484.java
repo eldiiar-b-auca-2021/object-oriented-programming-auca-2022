a435ba2e7b6cf027d7576ef61d59849844801968
eldiiar_b <eldiiar_b@auca.kg>
2022-02-21 23:31:29 +0600

    URI2484 wrong answer

---
+++ uri-midterm/src/URI2484.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class URI2484 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextLine()) {
+            String a = in.next();
+            int pos  = 1;
+            int len = a.length();
+            char[] ch = a.toCharArray();
+            for (int i = 0; i < ch.length; i++) {
+                for (int j = 0; j < len; j++) {
+                    if (j ==0) {
+                        System.out.printf("%" + pos + "s", ch[j]);
+                        j+=1;
+                    }
+
+                    System.out.print(" "+ch[j]);
+                }
+                pos+=1;
+                if (i == ch.length-1) {
+                    len-=2;
+                }
+                len--;
+                System.out.println();
+            }
+        }
+    }
+}
