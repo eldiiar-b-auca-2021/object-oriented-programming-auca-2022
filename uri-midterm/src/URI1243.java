295c98385da19c027a9a45639f6ff651029eaabe
eldiiar_b <eldiiar_b@auca.kg>
2022-02-28 20:41:01 +0600

    URI1243 wrong answer

--- uri-midterm/src/URI1243.java
+++ uri-midterm/src/URI1243.java
@@ -4,24 +4,54 @@ public class URI1243 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
-        int sum = 0;
-        int len = 0;
-        char[] a;
-        for (int i = 0; i < n; i++){
-            String first = in.next();
-            String second = in.next();
-            for (int j = 0; j < first.length(); j++) {
-                for (int k = 0; k < second.length(); j++) {
-                    if (first.substring(i).equals(second.substring(k))) {
-                        len++;
+        String letter;
+        while (in.hasNextLine()) {
+            letter = in.nextLine();
+            int count = 0;
+            int sum = 0;
+            int sum2 = 0;
+            String[] str = letter.split("\\s");
+            for (String r : str) {
+                for (int i = 0; i < r.length(); i++) {
+                    if (Character.isLetter(r.charAt(i) ) ) {
+                        count++;
+                    } else if (!Character.isLetter(r.charAt(r.length()-1)) && Character.isLetter(r.charAt(i))){
+                        count++;
+                    } else {
+                        count = 0;
                     }
                 }
+                if (count == r.length()) {
+                    sum++;
+                    sum2 += r.length();
+                }
+                count = 0;
             }
-            a = new char[len];
-            System.out.println(a.length);
-            len=0;
+
+
+
+            int result = 0;
+
+
+
+            if (sum == 0 || (sum2 / sum <= 3)) {
+                System.out.println("250");
+            } else {
+                result = sum2 / sum;
             }
+
+            if (result == 5 || result == 4) {
+                System.out.println("500");
+            } else if (result > 5) {
+                System.out.println("1000");
+            }
+
         }
+
+
+
+
+
     }
+}
 
