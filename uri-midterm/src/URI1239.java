98353d90d39073dac3870f636beea486342db4a6
eldiiar_b <eldiiar_b@auca.kg>
2022-03-01 11:26:19 +0600

    URI1239 accepted

--- uri-midterm/src/URI1239.java
+++ uri-midterm/src/URI1239.java
@@ -3,17 +3,13 @@ import java.util.Scanner;
 public class URI1239 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        int num = 2;
-        boolean test = true;
         while (in.hasNextLine()) {
-            String sentence = in.nextLine();
-            char[] words = sentence.toCharArray();
-            for (int i = 0; i < words.length; i++) {
-                if (words[i] == '_' && num % 2 == 0) {
+            String letter = in.nextLine();
+            letter = letter.replaceAll("_([^_]*)_","<i>$1</i>");
+            letter = letter.replaceAll("\\*([^*]*)\\*","<b>$1</b>");
 
-                }
-            }
+            System.out.println(letter);
         }
+
     }
 }
