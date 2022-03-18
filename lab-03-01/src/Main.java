b1dc47c7952b71087e36976ca6a4fe1dbd367997
eldiiar_b <eldiiar_b@auca.kg>
2022-03-18 22:06:28 +0600

    lab-03-01 completed

--- lab-03-01/src/Main.java
+++ lab-03-01/src/Main.java
@@ -1,24 +1,86 @@
 import java.util.*;
 
 public class Main {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+            while (true) {
+                try {
+                    Rational r1 = readRational("1st rational (EOF for exit): ");
+                    String op = readOperator("Enter operator (EOF for exit): ");
+                    Rational r2 = readRational("2nd rational (EOF for exit): ");
 
-        Rational a = Rational.parse(in.nextLine());
-        Rational b = Rational.parse(in.nextLine());
+                    System.out.printf("Result: %s %s %s = %s%n", r1, op, r2, compute(r1, r2, op));
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+    }
 
-        Rational sum = a.add(b);
-        Rational dif = a.subtract(b);
-        Rational multiply = a.multiply(b);
-        Rational divide = a.divide(b);
+    // stub
+    private static Object compute(Rational r1, Rational r2, String op) {
+        switch (op) {
+            case "+":
+                return r1.add(r2);
+            case "-":
+                return r1.subtract(r2);
+            case "*":
+                return r1.multiply(r2);
+            case "/":
+                return r1.divide(r2);
+            case "==":
+                return r1.compareTo(r2) == 0;
+            case "!=":
+                return r1.compareTo(r2) != 0;
+            case "<":
+                return r1.compareTo(r2) < 0;
+            case ">":
+                return r1.compareTo(r2) > 0;
+            case "<=":
+                return r1.compareTo(r2) <= 0;
+            default:
+                return r1.compareTo(r2) >= 0;
+        }
+    }
 
-        System.out.println(sum);
-        System.out.println(dif);
-        System.out.println(multiply);
-        System.out.println(divide);
-        System.out.printf("%s < %s = %b%n", a, b, a.compareTo(b) < 0);
-        System.out.printf("%s > %s = %b%n", a, b, a.compareTo(b) > 0);
+    private static String readOperator(String msg) {
+        while (true) {
+            System.out.print(msg);
+            if(!in.hasNextLine()){
+                System.exit(0);
+            }
+            String line = in.nextLine().trim();
 
-        System.out.println(Rational.count);
+            switch (line){
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "==":
+                case "!=":
+                case "<":
+                case ">":
+                case "<=":
+                case ">=":
+                    return line;
+                default:
+                    System.out.println("Incorrect operator: " + line);
+            }
+        }
+    }
+
+    private static Rational readRational(String msg) {
+        while (true) {
+            System.out.print(msg);
+            if(!in.hasNextLine()){
+               System.exit(0);
+            }
+            String line = in.nextLine().trim();
+            try {
+                return Rational.parse(line);
+            } catch (RuntimeException e){
+                System.out.println("Incorrect rational: " + line);
+            }
+        }
     }
 }
