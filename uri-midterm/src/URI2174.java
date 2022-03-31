61204ef13d6610d46b767370cc5ed7b1c5c78693
eldiiar_b <eldiiar_b@auca.kg>
2022-03-31 15:31:15 +0600

    URI2174 accepted

---
+++ uri-midterm/src/URI2174.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class URI2174 {
+    static final int TOTAL = 151;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        ArrayList<String> list = new ArrayList<>(num);
+        for (int i = 0; i < num; i++){
+            list.add(in.next());
+        }
+        Set<String> list1 = new HashSet<>(list);
+        int res = TOTAL - list1.size();
+        System.out.println("Falta(m) " + res + " pomekon(s).");
+
+    }
+}
