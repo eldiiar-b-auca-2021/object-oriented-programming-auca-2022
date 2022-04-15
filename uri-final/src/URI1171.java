0d73487b474e5de7150af1db4a9f584f25c24bf3
eldiiar_b <eldiiar_b@auca.kg>
2022-04-15 23:42:58 +0600

    URI1171 accepted

--- uri-final/src/URI1171.java
+++ uri-final/src/URI1171.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 public class URI1171 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
 
-        int num = scan.nextInt();
-        //StringBuilder collect = new StringBuilder();
-        ArrayList<Integer> ints = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < num; i++){
-            int N = scan.nextInt();
-            ints.add(N);
+        int n = inp.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int number = inp.nextInt();
+            list.add(number);
         }
-        Collections.sort(ints);
-        int length = ints.size()-count;
-        int[] set = new int[length];
-        int c = 0;
-        for (int i = 0; i < ints.size(); i++){
-            for (int j = 0; j < ints.size(); j++){
-                if (ints.get(i).equals(ints.get(j))){
-                    set[c]++;
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            int count = 1;
+            for (int j = 1+i; j < list.size(); j++){
+                if (Objects.equals(list.get(i),list.get(j))){
+                    list.remove(list.get(j));
+                    j-=1;
+                    count++;
                 }
-                c++;
             }
+            System.out.println(list.get(i) + " aparece " + count +" vez(es)");
         }
-        System.out.println(Arrays.toString(set));
     }
 }
