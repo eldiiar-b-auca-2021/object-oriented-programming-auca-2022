1d2fdc127a5590ac417945e4f83fda51cb7f9f49
eldiiar_b <eldiiar_b@auca.kg>
2022-04-29 18:34:49 +0600

    project-01 corrected

--- project-01/src/Main.java
+++ project-01/src/Main.java
@@ -5,29 +5,33 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         Game2048 game = new Game2048();
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++) {
-                int value = in.nextInt();
-                game.setAt(i,j,value);
-            }
-        }
+
         while (true){
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 4; j++) {
+                    int value = in.nextInt();
+                    game.setAt(i,j,value);
+                }
+            }
             int command = in.nextInt();
             switch(command){
                 case 0:
                     game.moveLeft();
+                    game.print();
                     break;
                 case 1:
-                    game.moveRight();
+                    game.moveUp();
+                    game.print();
                     break;
                 case 2:
-                    game.moveUp();
+                    game.moveRight();
+                    game.print();
                     break;
                 case 3:
                     game.moveDown();
+                    game.print();
                     break;
             }
-            game.display();
         }
     }
 }
