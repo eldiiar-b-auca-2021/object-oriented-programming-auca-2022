574143bd0dbe32894d925ddc008750384febd7ba
eldiiar_b <eldiiar_b@auca.kg>
2022-05-06 14:12:20 +0600

    URI1609 accepted

--- uri-final/src/URI1609.java
+++ uri-final/src/URI1609.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class URI1607 {
+public class URI1609 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
