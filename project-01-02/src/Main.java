ec8b1125ac3c7628e9666b5e862cdc7bea7ff443
eldiiar_b <eldiiar_b@auca.kg>
2022-05-18 16:16:40 +0600

    project-01 already completed

--- project-01-02/src/Main.java
+++ project-01-02/src/Main.java
@@ -24,6 +24,10 @@ public class Main extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
+        fill(255,255,0);
+        textSize(150);
+        strokeWeight(15);
+        text("Game 2048", width/4f, 100 );
         drawGameBoard();
     }
 
@@ -58,27 +62,57 @@ public class Main extends PApplet {
         translate(initialX, initialY);
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                switch(game.getCoordinates(i,j)){
-                    case 2 -> fill();
-
+                fill(204, 192, 179);
+                switch (game.getCoordinates(i, j)) {
+                    case 0:
+                        fill(204, 192, 179);
+                        break;
+                    case 2:
+                        fill(238, 228, 218);
+                        break;
+                    case 4:
+                        fill(237, 224, 200);
+                        break;
+                    case 8:
+                        fill(242, 177, 121);
+                        break;
+                    case 16:
+                        fill(245, 149, 99);
+                        break;
+                    case 32:
+                        fill(246, 124, 95);
+                        break;
+                    case 64:
+                        fill(246, 94, 59);
+                        break;
+                    case 128:
+                        fill(237, 207, 114);
+                        break;
+                    case 256:
+                        fill(237, 200, 80);
+                        break;
+                    case 512:
+                        fill(237, 197, 63);
+                        break;
+                    case 1024:
+                        fill(237, 194, 46);
+                        break;
+                    case 2048:
+                        fill(238, 288, 218, 73);
+                        break;
                 }
-                fill(155, 155, 155);
                 stroke(50);
                 strokeWeight(10);
                 square(0 + j * sizeOfTile + 10 * (j + 1), 0 + i * sizeOfTile + 10 * (i + 1), sizeOfTile);
-
-                stroke(50);
-                strokeWeight(10);
-                fill(255, 0, 0);
-                square(0 + j * sizeOfTile + 10 * (j + 1), 0 + i * sizeOfTile + 10 * (i + 1), sizeOfTile);
-
-
-                fill(0, 0, 255);
-                text(game.getCoordinates(i, j), 0 + j * sizeOfTile + 10 * (j + 1) + 30, 0 + i * sizeOfTile + 10 * (i + 1) + 100, sizeOfTile);
-
+                if (game.getCoordinates(i, j) == 0) {
+                    fill(204, 192, 179);
+                } else {
+                    textSize(100);
+                    fill(255, 50, 255);
+                    strokeWeight(15);
+                    text(game.getCoordinates(i, j), 0 + j * sizeOfTile + 10 * (j + 1) + sizeOfTile/2f-40, 0 + i * sizeOfTile + 10 * (i + 1) + sizeOfTile/2f+30, sizeOfTile);
+                }
             }
-            textSize(100);
-
         }
         popMatrix();
     }
